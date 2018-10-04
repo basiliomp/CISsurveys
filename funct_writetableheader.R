81b03452ec3561f34f3dce311ec520b9ad20f685
Basilio M. P <basiliomp@users.noreply.github.com>
2018-10-04 08:38:02 +0200

    Rename writetableheader_function.R to funct_writetableheader.R

--- funct_writetableheader.R
+++ funct_writetableheader.R
@@ -1,5 +1,5 @@
 # Tab with header function
-# Nueva función para darle un título a las tablas de recuerdo de voto
+# Nueva funciÃ³n para darle un tÃ­tulo a las tablas de recuerdo de voto
 write.table.header <- function(x, file, header){
   cat(header, '\n',  file = file)
   write.table(x= x, file = file, col.names = NA, sep = ";", dec = ",", append = T, row.names = T, na = "")
