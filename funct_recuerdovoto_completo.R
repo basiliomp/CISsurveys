8db7e9b71fd4d92aa04b31abb62f10f1930f00b7
Basilio M. P <basiliomp@users.noreply.github.com>
2018-10-09 10:18:35 +0200

    single_survey_transformation becomes a function

---
+++ funct_recuerdovoto_completo.R
@@ -0,0 +1,29 @@
+recuerdovoto_completo <- function(x) {  
+### Creating a complete voting behaviour variable combining two columns from each
+# For this project usage only!
+# This is not a general application function, and will not work out of the CISsurvey project.
+  
+  # Assign relevant variable into a dictinctly named new variable # Voto.reciente
+  CIS$Voto.reciente <- subset(CIS, select = general[[x, "Voto.reciente"]])
+  CIS$Voto.reciente <- as_vector(CIS$Voto.reciente)
+  
+  # Assign relevant variable into a dictinctly named new variable # Otro.reciente
+  CIS$Otro.reciente <- subset(CIS, select = general[[x, "Otro.reciente"]])
+  CIS$Otro.reciente <- as_vector(CIS$Otro.reciente)
+  
+  CIS$RECUERDO <- as.character(levels(CIS$Voto.reciente))[1]
+  
+  # Loop proposal for complete voting behaviour	# Loop proposal for complete voting behaviour
+  for (y in 1:nrow(CIS)) {
+    if (CIS[y, "Otro.reciente"][[1]] == "Fue a votar y vot.") {
+      CIS[y, "RECUERDO"][[1]] <- as.character(CIS[y, "Voto.reciente"][[1]])
+    } else if (CIS[y, "Otro.reciente"][[1]] == "S. que vot.") {  
+      CIS[y, "RECUERDO"][[1]] <- as.character(CIS[y, "Voto.reciente"][[1]])
+    } else if (CIS[y, "Otro.reciente"] == "N.C.") {
+      CIS[y, "RECUERDO"][[1]] <- "N.C. participacion"
+    } else {
+      CIS[y, "RECUERDO"][[1]] <- "Abstencion"
+    }
+  }
+  table(CIS$RECUERDO, CIS$Voto.reciente, useNA = "always")
+}
