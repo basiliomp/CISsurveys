57ae03d803993b72fb39226434a3647659ad1b94
basiliomp <basiliomperalta@gmail.com>
2018-10-15 22:28:13 +0200

    Voting intention caveats added to the votevarname funct
    to-do updated and exported to RMarkdown format.

--- funct_votevarname.R
+++ funct_votevarname.R
@@ -1,21 +1,31 @@
 #Function for creating explicit names to voting behaviour related variables
 
-  # ! Check what to do with these survey entries, because they are NAs!
-  filter(general, is.na(Elecciones)) %>%
-    select(1:3)
+# Problem to solve ----------------
+# ! Check what to do with these survey entries, because they are NAs!
+filter(general, is.na(Elecciones)) %>%
+  select(1:3)
 
+
+# Actual function -----------------
 #Extracting the year for each survey as simplified two characters version
 votevarname <- function (i) {
   if (i %in% 1:nrow(general)) { #Security check
-  preorpost <- general$Encuesta[[i]]
+    #Naming of pre or post surveys, according to the variable of interest (voting intention/behaviour)
+    if (general$Encuesta[[i]] == "pre") {
+      preorpost <- "INTE"
+      } else preorpost <- "RV"
+    #Type of election, regarding political arena (state level or regional one)
     if (general$Elecciones[[i]] == "Congreso") {
     elect_ambit <- "GEN"
-    } else elect_ambit <- "AUT"
+      } else elect_ambit <- "AUT"
   
+  #Extrac the year of the electoral event, in 2 digits format.
   simpleyear <-  str_extract(string = as.character(general$Year[[i]]), pattern = "..$")
+  
+  #Combine all the pieces of information together
   nombrevar <- paste0(preorpost, elect_ambit, simpleyear,  "AGR")
   return(nombrevar)
-  } else {
+  } else { #Error path for those number inputs without a reference in 'general'
     print("Invalid input. Use a row number from the reference database `general`")
   }
 }
